87b38ebef2dfd1dd78e27a874486ea5d28694676
Sashankr <sashankr97@gmail.com>
2025-02-15 15:13:33 +0530

    updated navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -5,7 +5,9 @@ const Navbar = () => {
     <nav className="navbar">
       <div className="container">
         <div className="navbar__wrapper">
-          <h2>TeeRex Store</h2>
+          <h2>
+            <i className="fa-solid fa-shop"></i> TeeRex Store
+          </h2>
           <section className="navbar__cart">
             <i className="fa-solid fa-cart-shopping"></i>
             <p className="navbar__cart-count">1</p>
